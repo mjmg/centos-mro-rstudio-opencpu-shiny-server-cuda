ca5fd1e35d31f7d90cc24e3914fd6d7a21f27479
mjmg <mjmg@users.noreply.github.com>
2017-05-26 17:05:42 +0800

    Use MRAN repo

--- installRcudapackages.sh
+++ installRcudapackages.sh
@@ -5,13 +5,13 @@
 #Rscript -e "install.packages('curl')"
 #echo "Installing httr from CRAN/MRAN"
 #Rscript -e "install.packages('httr')"
-echo "Installing testthat from CRAN/MRAN"
-Rscript -e "install.packages('testthat', repos='https://cloud.r-project.org/')"
+echo "Installing testthat from MRAN"
+Rscript -e "install.packages('testthat')"
 
-echo "Installing assertive from CRAN/MRAN"
-Rscript -e "install.packages('assertive', repos='https://cloud.r-project.org/')"
-echo "Installing RcppEigen from CRAN/MRAN"
-Rscript -e "install.packages('RcppEigen', repos='https://cloud.r-project.org/')"
+echo "Installing assertive from MRAN"
+Rscript -e "install.packages('assertive')"
+echo "Installing RcppEigen from MRAN"
+Rscript -e "install.packages('RcppEigen')"
 
 
 # This installs R packages from github
@@ -30,11 +30,11 @@ R CMD INSTALL gmatrix_0.3.tar.gz --no-test-load
 echo "Installing cdeterman/RViennaCL from github"
 Rscript -e "library(devtools); install_github('cdeterman/RViennaCL')"
 echo "Installing cdeterman/gpuR from github"
-#Rscript -e "library(devtools); install_github('cdeterman/gpuR', INSTALL_opts='--no-test-load')"
-git clone https://github.com/cdeterman/gpuR.git
-rm ./gpuR/.git -rf
-MAKE="make -j7" #note this make the compile process use 7 threads
-R CMD INSTALL gpuR --no-test-load
+Rscript -e "library(devtools); install_github('cdeterman/gpuR', INSTALL_opts='--no-test-load')"
+#git clone https://github.com/cdeterman/gpuR.git
+#rm ./gpuR/.git -rf
+#MAKE="make -j7" #note this make the compile process use 7 threads
+#R CMD INSTALL gpuR --no-test-load
 
 
 # This installs R packages in CRAN/MRAN
@@ -45,22 +45,22 @@ R CMD INSTALL gpuR --no-test-load
 #echo "Installing gcbd from CRAN"
 #Rscript -e "install.packages('gcbd')"
 
-echo "Installing cudaBayesreg from CRAN/MRAN"
-Rscript -e "install.packages('cudaBayesreg', repos='https://cloud.r-project.org/')"
-echo "Installing permGPU from CRAN/MRAN"
-Rscript -e "install.packages('permGPU', repos='https://cloud.r-project.org/')"
-echo "Installing snowfall from CRAN/MRAN"
-Rscript -e "install.packages('snowfall', repos='https://cloud.r-project.org/')"
-echo "Installing data.table from CRAN/MRAN"
-Rscript -e "install.packages('data.table', repos='https://cloud.r-project.org/')"
-echo "Installing bit64 from CRAN/MRAN"
-Rscript -e "install.packages('bit64', repos='https://cloud.r-project.org/')"
+echo "Installing cudaBayesreg from MRAN"
+Rscript -e "install.packages('cudaBayesreg')"
+echo "Installing permGPU from MRAN"
+Rscript -e "install.packages('permGPU')"
+echo "Installing snowfall from MRAN"
+Rscript -e "install.packages('snowfall')"
+echo "Installing data.table from MRAN"
+Rscript -e "install.packages('data.table')"
+echo "Installing bit64 from MRAN"
+Rscript -e "install.packages('bit64')"
 
 #echo "Installing gputools from source"
 #Rscript -e "library(devtools); install_local('/tmp/gputools_1.1.tar.gz', 'gputools')"
 
-echo "Installing gputools from CRAN/MRAN"
-Rscript -e "install.packages('gputools', repos='https://cloud.r-project.org/')"
+echo "Installing gputools from MRAN"
+Rscript -e "install.packages('gputools')"
 
 
 echo "Installing bigWig from github"
@@ -68,5 +68,5 @@ Rscript -e "library(devtools); install_github('mjmg/bigWig')"
 echo "Installing dREG from source"
 Rscript -e "library(devtools); install_github('mjmg/dREG')"
 echo "Installing Rgtsvm from source"
-Rscript -e "library(devtools); install_github('mjmg/Rgtsvm', args='--configure-args=--with-boost-home=/usr/lib64/microsoft-r/3.3/lib64/R/library/BH')"
+Rscript -e "library(devtools); install_github('Danko-Lab/Rgtsvm', args='--configure-args=--with-boost-home=/usr/lib64/microsoft-r/3.4/lib64/R/library/BH')"
 
