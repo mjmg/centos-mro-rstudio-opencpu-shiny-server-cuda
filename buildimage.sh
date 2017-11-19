713998017c095f914d9a85e612c139b77f829597
mjmg <mjmg@users.noreply.github.com>
2017-11-20 01:06:57 +0800

    Update for nvidia-docker2

--- buildimage.sh
+++ buildimage.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-NV_DOCKER='sudo docker -D' sudo nvidia-docker build -t mjmg/centos-mro-rstudio-opencpu-shiny-server-cuda .
+#NV_DOCKER='sudo docker -D' sudo nvidia-docker build -t mjmg/centos-mro-rstudio-opencpu-shiny-server-cuda .
+
+sudo nvidia-docker build -t mjmg/centos-mro-rstudio-opencpu-shiny-server-cuda .
 
 # Run Image
 #NV_DOCKER='sudo docker -D' sudo nvidia-docker run -p 80:80 -p 443:443 --name centos-mro-rstudio-opencpu-shiny-server-cuda -ti mjmg/centos-mro-rstudio-opencpu-shiny-server-cuda /bin/bash
